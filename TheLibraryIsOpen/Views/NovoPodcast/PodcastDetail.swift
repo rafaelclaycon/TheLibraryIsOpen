b3f706a396a86ef8783a3a9d69940b68b9fdcf1d
Rafael Claycon Schmitt <rafael.claycon@gmail.com>
2021-03-19 11:01:39 -0300

    feat: Nova tela de detalhes para baixar podcast

--- TheLibraryIsOpen/Views/NovoPodcast/PodcastDetail.swift
+++ TheLibraryIsOpen/Views/NovoPodcast/PodcastDetail.swift
@@ -10,69 +10,57 @@ import SwiftUI
 
 struct PodcastDetail: View {
     @ObservedObject var viewModel: PodcastDetailViewModel
+    @State private var indicePagina = 0
 
     var body: some View {
-        VStack(alignment: .center) {
-            // Header
-            KFImage(URL(string: viewModel.artworkURL))
-                .onSuccess { r in
-                    print("success: \(r)")
-                }
-                .onFailure { e in
-                    print("failure: \(e)")
-                }
-                .placeholder {
-                    ZStack {
-                        Rectangle()
-                            .fill(Color.gray)
-                            .frame(width: 70, height: 70)
-                            .opacity(0.5)
-
-                        Image(systemName: "waveform")
-                            .resizable()
-                            .frame(width: 35, height: 35)
-                            .foregroundColor(.white)
-                            .opacity(0.5)
+        VStack {
+            HStack {
+                KFImage(URL(string: viewModel.artworkURL))
+                    .onSuccess { r in
+                        print("success: \(r)")
                     }
+                    .onFailure { e in
+                        print("failure: \(e)")
+                    }
+                    .placeholder {
+                        ZStack {
+                            Rectangle()
+                                .fill(Color.gray)
+                                .frame(width: 70, height: 70)
+                                .opacity(0.5)
+
+                            Image(systemName: "waveform")
+                                .resizable()
+                                .frame(width: 35, height: 35)
+                                .foregroundColor(.white)
+                                .opacity(0.5)
+                        }
+                    }
+                    .resizable()
+                    .frame(width: 55, height: 55)
+                
+                VStack(alignment: .leading) {
+                    Text(viewModel.title)
+                        .font(.title3)
+                        .bold()
+                    Text(viewModel.details)
+                        .font(.subheadline)
+                        .multilineTextAlignment(.center)
+                        .padding(.top, 2)
                 }
-                .resizable()
-                .frame(width: 170, height: 170)
-                .cornerRadius(8.0)
-            
-            Text(viewModel.title)
-                .font(.title)
-                .bold()
-                .multilineTextAlignment(.center)
-                .padding(.horizontal, 10)
-                .padding(.top, 12)
-            
-            Text(viewModel.details)
-                .foregroundColor(.gray)
-                .font(.subheadline)
-                .multilineTextAlignment(.center)
-                .padding(.all, 5)
-            
-            Button(action: {
-                viewModel.downloadAll()
-            }) {
-                Image(systemName: "icloud.and.arrow.down")
-                    .font(Font.body.weight(.bold))
-                    .padding(.trailing, 4)
-                Text("Baixar tudo")
-                    .bold()
+                .padding(.leading, 15)
             }
-            .padding(.vertical, 15)
+            .padding(.horizontal, 15)
+            
+            Picker(selection: $indicePagina, label: Text("Info")) {
+                Text("Lista").tag(0)
+                Text("Por ano").tag(1)
+            }
+            .pickerStyle(SegmentedPickerStyle())
             .padding(.horizontal, 25)
-            .foregroundColor(.white)
-            .background(Color.accentColor)
-            .cornerRadius(30)
-            .alert(isPresented: $viewModel.displayAlert) {
-                Alert(title: Text(viewModel.alertTitle), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
-            }
-            .padding(.vertical, 10)
+            .padding(.vertical, 15)
             
             Divider()
-                .padding(.top, 15)
 
             // List
             if viewModel.displayEpisodeList {
@@ -95,6 +83,30 @@ struct PodcastDetail: View {
                     Spacer()
                 }
             }
+            
+            Divider()
+                .padding(.bottom, 5)
+            
+            Button(action: {
+                viewModel.downloadAll()
+            }) {
+                Text("Baixar 0 episódios (0 GB)")
+                    .bold()
+            }
+            .padding(.vertical, 15)
+            .padding(.horizontal, 25)
+            .foregroundColor(.white)
+            .background(Color.accentColor)
+            .cornerRadius(30)
+            .alert(isPresented: $viewModel.displayAlert) {
+                Alert(title: Text(viewModel.alertTitle), message: Text(viewModel.alertMessage), dismissButton: .default(Text("OK")))
+            }
+            .padding(.vertical, 5)
+            
+            Text("Você pode baixar todos os episódios neste dispositivo, porém restará menos de 10% do espaço livre atualmente (40,5 GB).")
+                .font(.subheadline)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 25)
         }
     }
 }
